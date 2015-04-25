0520c9b501943b03f6d15896d1719f6706702147
stavcd <stavcd@mail.ru>
2015-04-25 22:20:20 +0300

    added custom validators for card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,19 @@
 class Card < ActiveRecord::Base
   validates :original_text, :translated_text, :review_date, presence: true
+  validate :validate_text
+  before_create do
+    self.review_date = self.review_date + 3.day
+  end
+
+  def validate_text
+    if word(original_text)==word(translated_text)
+      self.errors.add(:original_text, 'Оригинальный текст не должен быть равен тексту перевода')
+      self.errors.add(:translated_text, 'Текст перевода не должен быть равен  оригинальному тексту')
+    end
+  end
+
+  def word(word_param)
+    word_param.squish.mb_chars.downcase.to_s
+  end
+
 end
