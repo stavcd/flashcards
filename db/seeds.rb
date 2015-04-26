90097690c311b2b2c631aff0281b37b9102bbcb0
stavcd <stavcd@mail.ru>
2015-04-26 22:47:34 +0300

    correction working nokogiri  with encoding utf-8

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-# ruby encoding: utf-8
-
 require 'open-uri'
 require 'nokogiri'
 
@@ -7,9 +5,12 @@ Card.delete_all
 
 url = "https://quizlet.com/45155485/flash-cards"
 
-doc = Nokogiri::HTML(open(url))
+doc = Nokogiri::HTML(open(url), nil, 'utf-8')
 doc.css('div.text').each do |word|
   original_text = word.css('span.lang-en').text
   translated_text = word.css('span.lang-ru').text
-  Card.create!(original_text: original_text, translated_text: translated_text, review_date: DateTime.now)
+  Card.create! original_text: original_text,
+               translated_text: translated_text,
+               review_date: DateTime.now
+
 end
