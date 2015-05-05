1f094505bde8f513233f153755e69b2720577ea5
stavcd <stavcd@mail.ru>
2015-05-05 23:21:13 +0300

    correction feature specs

--- spec/features/review_card_spec.rb
+++ spec/features/review_card_spec.rb
@@ -4,15 +4,20 @@ feature 'Review Card', 'It should be the ability to check the knowledge Card' do
 
   before do
     Card.create(original_text: 'hello', translated_text: 'Привет', review_date: '01.05.2015')
+    visit root_path
   end
 
   scenario 'enter the correct translation' do
-    visit root_path
     fill_in 'input_text', with: 'Привет'
     click_on 'Проверить'
     expect(page).to have_content 'Правильный перевод'
   end
 
+  scenario 'enter the incorrect translation' do
+    fill_in 'input_text', with: 'Приветик'
+    click_on 'Проверить'
+    expect(page).to have_content 'Неправильный перевод'
+  end
 
 end
 
