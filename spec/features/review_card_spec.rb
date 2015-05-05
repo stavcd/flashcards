559483ceacbee9832a38a7e2cf2537efe73dc89a
stavcd <stavcd@mail.ru>
2015-05-05 19:01:16 +0300

    add feature review card

--- spec/features/review_card_spec.rb
+++ spec/features/review_card_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
-feature 'Review Card', %q{'It should
-  be the ability to
-  check the knowledge Card'} do
+feature 'Review Card', 'It should be the ability to check the knowledge Card' do
+
+  before do
+    Card.create(original_text: 'hello', translated_text: 'Привет', review_date: '01.05.2015')
+  end
+
+  scenario 'enter the correct translation' do
+    visit root_path
+    fill_in 'input_text', with: 'Привет'
+    click_on 'Проверить'
+    expect(page).to have_content 'Правильный перевод'
+  end
+
 
 end
 
