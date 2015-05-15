8a34071cb319d94186579bdbc798fe8c87b06bd4
Stavcd <stavcd@mail.ru>
2015-05-15 14:54:58 +0300

    error correction

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      provider: 'AWS'
-  }
-  config.fog_directory = Rails.application.secrets.aws_fog_directory
-  config.fog_public = true
+  unless Rails.env.test
+    config.fog_credentials = {
+        aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+        aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+        provider: 'AWS'
+    }
+    config.fog_directory = Rails.application.secrets.aws_fog_directory
+    config.fog_public = true
+  end
 end
